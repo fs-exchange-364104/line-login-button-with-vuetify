b2b1ffd8bd7a5f61d976600aa77906a300439bfc
tpywao <tpywao@gmail.com>
2018-06-16 19:59:14 +0900

    Remove comment

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -4,21 +4,6 @@ const webpack = require('webpack')
 module.exports = {
   module: {
     rules: [
-      // {
-      //   test: /\.css$/,
-      //   use: [
-      //     'vue-style-loader',
-      //     'css-loader'
-      //   ],
-      // },
-      // {
-      //   test: /\.sass$/,
-      //   use: [
-      //     'vue-style-loader',
-      //     'css-loader',
-      //     'sass-loader?indentedSyntax'
-      //   ],
-      // },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
@@ -45,28 +30,8 @@ module.exports = {
           name: 'img/[name].[hash:7].[ext]'
         }
       },
-      // {
-      //   test: /\.(png|jpg|gif|svg)$/,
-      //   loader: 'file-loader',
-      //   options: {
-      //     name: '[name].[ext]?[hash]'
-      //   }
-      // },
     ]
   },
-  // externals: {
-  //   vue: {
-  //     // commonjs: 'vue',
-  //     // commonjs2: 'vue',
-  //     // amd: 'vue',
-  //     root: 'Vue',
-  //   },
-  // },
-  // plugins: [
-  //   new webpack.ProvidePlugin({
-  //     'Vue': 'vue',
-  //   }),
-  // ],
   resolve: {
     alias: {
       '@': path.resolve('src'),
